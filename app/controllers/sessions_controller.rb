ead9d4ec6285c7fc3ac51ba8ea176282fb869c5c
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-09-09 13:03:25 +0000

    a little correction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
     #フォームから送信されたメールアドレスを取得し、一致するユーザーがいるか検索
     if user && user.authenticate(params[:session][:password])
     #ユーザーのパスワードが正しいかどうか確かめている
-    #街灯のメールアドレスをもつuserが存在している　かつuserのパスワードが正しい場合にtrueになる
-      log_in user
+    #該当のメールアドレスをもつuserが存在している　
+          #かつuserのパスワードが正しい場合にtrueになる
+      log_in(user)
       redirect_to root_path, success: 'ログインに成功しました'
     else
       frash.now[:danger] = 'ログインに失敗しました'
@@ -20,13 +21,13 @@ class SessionsController < ApplicationController
     log_out
     redirect_to root_url, info: 'ログアウトしました'
   end
-  
+
   private
   def log_in(user)
-    session[:user_id] = user.id　#log_inメソッドでsession[:user_id]に
+    session[:user_id] = user.id  #log_inメソッドでsession[:user_id]に
                                  #ログイン情報を暗号化して保存
   end
-  
+
   def log_out
     session.delete(:user_id) #Railsにもともと定義されているメソッド
     @current_user = nil
