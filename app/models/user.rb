75bc1ee90a8da8365d0b8749d627ffe841de8433
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-09-15 02:45:53 +0000

    Create a favorite page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
-  validates :email, presence: true, format: { with: /\A[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]\z/ }
+  validates :email, presence: true, format: { with: /\A[A-Za-z0-9._+]*+@[A-Za-z]*+.[A-Za-z]*\z/ }
   validates :password, format: { with: /\A^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}$\z/ }
   
   has_secure_password
