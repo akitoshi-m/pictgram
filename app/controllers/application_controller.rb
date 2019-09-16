498b09b976ea908c32d0d103d8b1668f22307dd8
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-09-16 04:41:02 +0000

    Correction of Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
-#  include ApplicationHelper
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
