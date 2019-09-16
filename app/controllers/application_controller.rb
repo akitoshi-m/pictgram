edebd855ba9e9777c07f480d942242e2e5128e0e
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-09-16 02:24:21 +0000

    Correction of commonaction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
 
-  include CommonActions
-  
-  before_action :current_user
-  before_action :logged_in?
+  include ApplicationHelper
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
-  include CommonActions
-  
-  before_action :current_user
-  before_action :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in? #ログインしているかどうかを確認するメソッド
+    !current_user.nil?
+  end
+
 end
